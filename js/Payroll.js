4ffa890fe90775e9be6d6cc2496b51084b68dc82
pbishal <pbishal60@gmail.com>
2021-06-06 21:10:33 +0530

    Able to Create Employee Payroll Object On save

--- js/Payroll.js
+++ js/Payroll.js
@@ -21,4 +21,66 @@ window.addEventListener('DOMContentLoaded', (event) => {
     salary.addEventListener('input', function () {
          output.textContent = salary.value;
     });
+
+    const date = document.querySelector('#date');
+    date.addEventListener('input', function () {
+        let startDate = document.querySelector('#day').value + " " + document.querySelector('#month').value + " " +
+            document.querySelector('#year').value;
+        try {
+            (new EmployeePayroll()).startDate = new Date(Date.parse(startDate));
+            setTextValue('.date-error', "");
+        } 
+        catch (e) {
+            setTextValue('.date-error', e);
+        }
+    });
 });
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+// UC 3
+const save = () => {
+    try {
+        let EmployeePayRoll = createEmployeePayroll();
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayroll();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    }
+    catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') + "," + getInputValueById('#month') + "," + getInputValueById('#year');
+    employeePayrollData.startDate = new Date(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let sellItems = [];
+    allItems.forEach(item => {
+        if (item.checked)
+            sellItems.push(item.value);
+    });
+    return sellItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
